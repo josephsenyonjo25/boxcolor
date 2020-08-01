1e41a3433439b087316a0cc8f2fcb48b3ea3193f
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-08-01 07:37:55 -0400

    added state

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,33 +3,18 @@ import React, {Component} from 'react';
 class Box extends Component {
     constructor(props) {
         super(props);
-  
+
         this.state= {
             color:"blue",
             clickCounter: 0
         };
   
-        this.handleClick = this.handleClick.bind(this);
-    
-      }
-
-    handleClick() {
-        let boxcolor;
-        if(this.state.clickCounter %2 === 0) {
-            boxcolor = "blue";
-        }
-        else{
-            boxcolor = "orange";
-        }
-        let count = this.state.clickCounter+1;
-        this.setState({clickCounter: count, color: boxcolor});
-    }
-
+        
         render() {
           return (
             <div className= "Box">
                 <h1>Box color change</h1>
-                    <div className = "box" style ={{backgroundColor: this.state.color}} onClick={()=>this.handleClick()}>
+                    <div className = "box" style ={{backgroundColor: this.state.color}} onClick={()=>this.toggleColor()}>
                         <p className="name">Click Here</p>
                     </div>
                     
